0abee7914aff06e9700cc96a4b4b0c2196a21f86
ayush7088 <ayushjoshi282@gmail.com>
2023-06-25 21:23:43 +0530

    feat:added chat and notification icons.

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,30 +1,47 @@
 import "./topbar.css";
 import SearchIcon from '@mui/icons-material/Search';
-
+import PersonIcon from '@mui/icons-material/Person';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 export default function Topbar() {
   return (
     <div className="topbarContainer">
-        <div className="topbarLeft">
-          <span className="logo">Friendify</span>
+      <div className="topbarLeft">
+        <span className="logo">Friendify</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchBar">
+          <SearchIcon />
+
+          <input placeholder="Search for posts,friends,socials,anything..." className="searchInput" />
+
         </div>
-        <div className="topbarCenter">
-          <div className="searchBar">
-            <SearchIcon />
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">HomePage</span>
+          <span className="topbarLink">TimeLine</span>
 
-            <input placeholder="Search for posts,friends,socials,anything..." className="searchInput"/>
-
-          </div>
         </div>
-        <div className="topbarRight">
-          <div className="topbarLinks">
-            <span className="topbarLink">HomePage</span>
-            <span className="topbarLink">TimeLine</span>
-
-          </div>
-          <div className="topbarIcons"></div>
+        <div className="topbarIcons"></div>
+        <div className="topbarIconItem">
+          <PersonIcon />
+          <span className="topbarIconBadge">1</span>
         </div>
 
+        <div className="topbarIconItem">
+          <ChatIcon />
+          <span className="topbarIconBadge">2</span>
+        </div>
+
+        <div className="topbarIconItem">
+          <NotificationsIcon />
+          <span className="topbarIconBadge">1</span>
+        </div>
+
+      </div>
+      <div className="topbarIcons"></div>
     </div>
   )
 }
