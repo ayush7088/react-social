664735c6593a0e01f67f33015005439fa09a0570
ayush7088 <ayushjoshi282@gmail.com>
2023-06-26 08:46:39 +0530

    feat: more functionalities

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,20 @@
 import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/Rightbar/Rightbar";
+import "./Home.css"
+
+
 
 export default function Home() {
   return (
-    <div>
-        <Topbar />
-    </div>
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
   )
 }
