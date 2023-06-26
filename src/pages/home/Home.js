f4697e1aadaa877540d7f91ce7a95922cba68f41
ayush7088 <ayushjoshi282@gmail.com>
2023-06-26 12:37:14 +0530

    fix: name of sidebar,rightbar folder

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,7 @@
 import Topbar from "../../components/topbar/Topbar";
-import Sidebar from "../../components/Sidebar/Sidebar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
-import Rightbar from "../../components/Rightbar/Rightbar";
+import Rightbar from "../../components/rightbar/Rightbar";
 import "./Home.css";
 
 
