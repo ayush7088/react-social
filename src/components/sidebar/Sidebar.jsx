8318534bf876601e9856b04a31038c3a6473ce9b
ayush7088 <ayushjoshi282@gmail.com>
2023-06-29 11:37:57 +0530

    fix: fixed flex

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,22 @@
 import "./sidebar.css"
+import FeedIcon from '@mui/icons-material/Feed';
+import ChatIcon from '@mui/icons-material/Chat';
 
 export default function Sidebar() {
   return (
-    <div className="sidebar">Sidebar</div>
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+          <FeedIcon className="sidebarIcon"/>
+          <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+          <ChatIcon className="sidebarIcon"/>
+          <span className="sidebarListItemText">Chats</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
